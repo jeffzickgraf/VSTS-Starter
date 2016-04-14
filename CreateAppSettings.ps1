a9d48945565cdadf054d5d29ab6e103454419226
Jeff Zickgraf <jeffzickgraf@gmail.com>
2016-04-14 10:30:02 -0400

    Adding CreateAppSettings for build process on VSTS.

---
+++ CreateAppSettings.ps1
@@ -0,0 +1,28 @@
+﻿#used to create an AppSettings file on the VSTS build server during a build. Takes in credentials and the cloud location
+
+param([string] $perfectoCloud = "", [string] $perfectoUsername = "", [string] $perfectoPassword = "")
+
+# Create a new XML File with config root node
+[System.XML.XMLDocument]$oXMLDocument=New-Object System.XML.XMLDocument
+# New Node
+[System.XML.XMLElement]$oXMLRoot=$oXMLDocument.CreateElement("appSettings")
+# Append as child to an existing node
+$oXMLDocument.appendChild($oXMLRoot)
+
+# Add cloud key 
+[System.XML.XMLElement]$oXMLCloud=$oXMLRoot.appendChild($oXMLDocument.CreateElement("add"))
+$oXMLCloud.SetAttribute("key","PerfectoCloud")
+$oXMLCloud.SetAttribute("value", $perfectoCloud)
+
+# Add username key 
+[System.XML.XMLElement]$oXMLUser=$oXMLRoot.appendChild($oXMLDocument.CreateElement("add"))
+$oXMLUser.SetAttribute("key","PerfectoUsername")
+$oXMLUser.SetAttribute("value", $perfectoUsername)
+
+# Add password key 
+[System.XML.XMLElement]$oXMLPassword=$oXMLRoot.appendChild($oXMLDocument.CreateElement("add"))
+$oXMLPassword.SetAttribute("key","PerfectoPassword")
+$oXMLPassword.SetAttribute("value", $perfectoPassword)
+
+# Save File
+$oXMLDocument.Save("VSTSDigitalDemoTests\appsettings.config")
