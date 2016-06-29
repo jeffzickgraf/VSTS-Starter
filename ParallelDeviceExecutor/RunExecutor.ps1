65888842336ca8ffee711d02a8cb9b5fa3906c86
Jeff Zickgraf <jeffzickgraf@gmail.com>
2016-06-29 15:59:41 -0400

    Reworking Start-Process in powershell RunExecutor script.

--- ParallelDeviceExecutor/RunExecutor.ps1
+++ ParallelDeviceExecutor/RunExecutor.ps1
@@ -1,17 +1,5 @@
-
 param([String] $assemblies = "", [String] $wherestatement="", [String] $baselocation="C:\a\1\s\")
-$psi = New-object System.Diagnostics.ProcessStartInfo 
-$psi.CreateNoWindow = $true 
-$psi.UseShellExecute = $false 
-$psi.WorkingDirectory = $baselocation + 'ParallelDeviceExecutor\bin\Release'
-$psi.RedirectStandardOutput = $true 
-$psi.RedirectStandardError = $true 
-$psi.FileName = $baselocation + 'ParallelDeviceExecutor\bin\Release\ParallelDeviceExecutor.exe'
-$psi.Arguments ="a:$assemblies w:$wherestatement"
-$process = New-Object System.Diagnostics.Process 
-$process.StartInfo = $psi 
-$process.Start()
-$output = $process.StandardOutput.ReadToEnd() 
-$process.WaitForExit() 
-$output
 
+$filePath = $baselocation + 'ParallelDeviceExecutor\bin\Release\ParallelDeviceExecutor.exe'
+$workingDirectory = $baselocation + 'ParallelDeviceExecutor\bin\Release'
+Start-Process -FilePath $filePath -Args "a:$assemblies", "w:$wherestatement" -NoNewWindow -PassThru -Wait -WorkingDirectory $workingDirectory
