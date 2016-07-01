257a76ef861cba3aa768938c6eed4b0fc34bbfc3
Jeff Zickgraf <jeffzickgraf@gmail.com>
2016-07-01 11:51:05 -0400

    Another run executor try.

--- ParallelDeviceExecutor/RunExecutor.ps1
+++ ParallelDeviceExecutor/RunExecutor.ps1
@@ -2,4 +2,6 @@ param([String] $assemblies = "", [String] $wherestatement="", [String] $baseloca
 
 $filePath = $baselocation + 'ParallelDeviceExecutor\bin\Release\ParallelDeviceExecutor.exe'
 $workingDirectory = $baselocation + 'ParallelDeviceExecutor\bin\Release'
-Start-Process -FilePath $filePath -Args "a:"$assemblies, "w:"$wherestatement -NoNewWindow -PassThru -Wait -WorkingDirectory $workingDirectory
+$arguments = "a:" + $assemblies + " w:" + $wherestatement
+#& C:\Users\jeffz\Source\Playground\arguments\EchoArgs.exe "a:$assemblies", "w:$wherestatement" 
+Start-Process -FilePath $filePath -Args "a:$assemblies", "w:$wherestatement" -NoNewWindow -PassThru -Wait -WorkingDirectory $workingDirectory
