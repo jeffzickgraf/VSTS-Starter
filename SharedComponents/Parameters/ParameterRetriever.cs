3b4638a03708ac21eca9cc53015e22f47c3e3303
Jeff Zickgraf <jeffzickgraf@gmail.com>
2016-07-27 20:54:44 -0400

    Reworked ParameterRetriever.cs to use linq to json instead of serialization to address issue with on prem TFS deserialization errors.

--- SharedComponents/Parameters/ParameterRetriever.cs
+++ SharedComponents/Parameters/ParameterRetriever.cs
@@ -1,7 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace SharedComponents.Parameters
 {
@@ -12,10 +13,9 @@ namespace SharedComponents.Parameters
 	{
 		public PerfectoTestParams GetVSOExecParam(string baseProjectPath = "", bool UseBuildDropFolder = true)
 		{
-			List <PerfectoTestParams> parameters = new List<PerfectoTestParams>();
-			List<string> devices = new List<string>();
 			PerfectoTestParams testParams = null;
-						
+			List<Device> devices = new List<Device>();
+
 			try
 			{
 				//We either get the device list via VSTS passing in a selected json
@@ -28,29 +28,14 @@ namespace SharedComponents.Parameters
 				string finalResolutionPath = vSTSConfigJSONPath;
 				if (!UseBuildDropFolder || !File.Exists(vSTSConfigJSONPath))
 				{
-					//Get file from local machine build
-
-					//When the MultiTestExecutor is used, need to take from test run folder otherwise - need to grab from sharedcomponents
-					if (currentPath.Contains("TestRuns"))
-					{
-						baseProjectPath = currentPath + @"\TestResources\DevicesGroup\";
-					}
-					else
-					{
-						baseProjectPath += @"\SharedComponents\TestResources\DevicesGroup\";
-					}
-
-					finalResolutionPath = baseProjectPath +  SharedConstants.DeviceConfigFileName;
+					finalResolutionPath = GetFinalResolutionOfPath(ref baseProjectPath, currentPath);
 
 					Console.WriteLine("VSTS file not found - pulling from: " + finalResolutionPath);
-				}							
-
-				//Deserialize params file
-				using (StreamReader jsonConfigFile = File.OpenText(finalResolutionPath))				
-				{
-					JsonSerializer serializer = new JsonSerializer();
-					testParams = (PerfectoTestParams)serializer.Deserialize(jsonConfigFile, typeof(PerfectoTestParams));
 				}
+				
+				InflateDeviceList(devices, finalResolutionPath);
+
+				testParams = new PerfectoTestParams(devices, string.Empty, string.Empty, string.Empty);
 			}
 			catch (Exception e)
 			{
@@ -58,6 +43,107 @@ namespace SharedComponents.Parameters
 				throw;
 			}
 			return testParams;
+		}
+
+		private static void InflateDeviceList(List<Device> devices, string finalResolutionPath)
+		{
+			/*
+			* Jeff Zickgraf's HACK Note:
+			*
+			* Previous code was nice and simple:
+			*	using (StreamReader jsonConfigFile = File.OpenText(finalResolutionPath))				
+			{
+				JsonSerializer serializer = new JsonSerializer();
+				testParams = (PerfectoTestParams)serializer.Deserialize(jsonConfigFile, typeof(PerfectoTestParams));
+			}
+			*
+			* However, deserialization on DoS's TFS instance isn't working as expected for some reason
+			* On VSTS we never have seen any issue.
+			* I tried using both .NET serialization/deserialization as well as JSON.NET
+			* and get intermitent deserialization errors. Don't have access to their TFS box to troubleshoot
+			* So going to inflate to our DeviceList object with Linq to JSON with JSON.NET: http://www.newtonsoft.com/json/help/html/LINQtoJSON.htm
+			*/
+
+			string deviceJSONString = File.ReadAllText(finalResolutionPath);
+			JObject devicesObject = JObject.Parse(deviceJSONString);
+			var devicesList = devicesObject.Children().Children().Children()["device"].ToList();
+
+			//For each content object create a new device instance.			
+			foreach (JObject content in devicesList)
+			{
+				string deviceID = string.Empty;
+				string os = string.Empty;
+				string osVersion = string.Empty;
+				string browserName = string.Empty;
+				string browserVersion = string.Empty;
+				string name = string.Empty;
+				bool isDesktopBrowser = false;
+				bool runNative = false;
+				bool runWeb = false;
+
+				Console.WriteLine(">>>>>>>>>>>>>>>>>Create Device Object ");
+
+				foreach (JProperty prop in content.Properties())
+				{
+					switch (prop.Name)
+					{
+						case "deviceID":
+							deviceID = prop.Value.ToString();
+							break;
+						case "os":
+							os = prop.Value.ToString();
+							break;
+						case "osVersion":
+							osVersion = prop.Value.ToString();
+							break;
+						case "browserName":
+							browserName = prop.Value.ToString();
+							break;
+						case "browserVersion":
+							browserVersion = prop.Value.ToString();
+							break;
+						case "name":
+							name = prop.Value.ToString();
+							break;
+						case "isDesktopBrowser":
+							isDesktopBrowser = (bool)prop.Value;
+							break;
+						case "runNative":
+							runNative = (bool)prop.Value;
+							break;
+						case "runWeb":
+							runWeb = (bool)prop.Value;
+							break;
+						default:
+							Console.WriteLine("Unknown JSON Property while deserializing Device List of: " + prop.Name);
+							break;
+					}
+					Console.WriteLine(prop.Name + " " + prop.Value);
+				}
+				Device device = new Device();
+				device.DeviceDetails = new DeviceDetails(deviceID, os, osVersion, name, browserName,
+						browserVersion, isDesktopBrowser, runNative, runWeb);
+				devices.Add(device);
+			}
+		}
+
+		private static string GetFinalResolutionOfPath(ref string baseProjectPath, string currentPath)
+		{
+			string finalResolutionPath;
+			//Get file from local machine build
+
+			//When the MultiTestExecutor is used, need to take from test run folder otherwise - need to grab from sharedcomponents
+			if (currentPath.Contains("TestRuns"))
+			{
+				baseProjectPath = currentPath + @"\TestResources\DevicesGroup\";
+			}
+			else
+			{
+				baseProjectPath += @"\SharedComponents\TestResources\DevicesGroup\";
+			}
+
+			finalResolutionPath = baseProjectPath + SharedConstants.DeviceConfigFileName;
+			return finalResolutionPath;
 		}
 	}
 }
